9e44b2ecb2fbd3a41e14585dfce3ad451772399a
Manika Sharma <51908554+manikasharma09@users.noreply.github.com>
2019-06-26 15:14:20 +0530

    Delete celery.py

--- app/celery.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_redis.settings') 
-
-app = Celery('django_celery_redis')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
